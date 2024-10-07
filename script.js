2d6d5db4abb7cf50cb4a38bb65d7c94eb7b5138f
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-10-07 22:26:55 +0600

    finished all

--- script.js
+++ script.js
@@ -1,33 +1,4 @@
-// // script.js
 
-// // Simple Modal
-// const modal = document.getElementById('modal');
-// const modalTrigger = document.getElementById('modal-trigger');
-
-// modalTrigger.addEventListener('click', () => {
-//     modal.classList.add('show');
-// });
-
-// modal.addEventListener('click', (e) => {
-//     if (e.target === modal) {
-//         modal.classList.remove('show');
-//     }
-// });
-
-// // Smooth Scroll
-// const scrollLinks = document.querySelectorAll('a[href^="#"]');
-
-// scrollLinks.forEach((link) => {
-//     link.addEventListener('click', (e) => {
-//         e.preventDefault();
-//         const href = link.getAttribute('href');
-//         const offsetTop = document.querySelector(href).offsetTop;
-//         window.scrollTo({
-//             top: offsetTop,
-//             behavior: 'smooth',
-//         });
-//     });
-// });
 
 const modal = document.getElementById('modal');
 const modalTrigger = document.getElementById('modal-trigger');
